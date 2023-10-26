366d1f98b40a18f84bd2430fd710073d0c1e3f09
Abraham Elijah <gradeisageek@gmail.com>
2023-10-27 00:18:11 +0100

    Update Income.js

--- bizly-backend/models/Income.js
+++ bizly-backend/models/Income.js
@@ -1,4 +1,16 @@
+#!/usr/bin/env node 
+
+// This module contains:
+//    - the Model for Income
+//    - Function to validate the Income object
+
+
+// importing the required libraries
 const mongoose = require('mongoose');
+const Joi = require('joi');
+
+
+// Creating a new Schema
 const Schema = mongoose.Schema;
 
 const incomeSchema = new Schema({
@@ -7,15 +19,40 @@ const incomeSchema = new Schema({
     required: true,
     unique: true
   },
-  date: Date.now(),
-  
+  date_created: {
+    type: mongoose.Schema.Types.Date,
+    default: Date.now()
+  },
+  businessId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Business',
+    required: true
+  },
+  source: {
+    type: String,
+    required: true,
+  }
 
 });
 
 
-// Creating a User Model from the Schema above
-const User = mongoose.model('user', userSchema);
+// Creating an Income Class from the Schema above
+const Income = mongoose.model('Income', incomeSchema);
+
+
+// Function to Validate the Income Object
+function validateIncome(income) {
+  const schema = {
+    amount: Joi.number().required(),
+    date_created: Joi.date(),
+    source: Joi.string().required()
+
+  };
+
+  return Joi.validate(income, schema);
+}
 
 
 // Exporting Model
-exports = User;
+module.exports.Income = Income;
+module.exports.validateIncome = validateIncome;
